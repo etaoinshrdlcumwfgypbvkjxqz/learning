2539d80879577f0bcafc4094ce4eafebf1cd2a86
William So <williamso26144225@gmail.com>
2021-01-22 09:12:28 +0800

    Setup project
    Signed-off-by: William So <williamso26144225@gmail.com>

---
+++ C++/HKOI/S191/unstoppable_onslaught.cpp
@@ -0,0 +1,13 @@
+#include "unstoppable_onslaught.h"
+
+// copy from here
+#include <iostream>
+
+using namespace std;
+
+namespace s191 {
+    int main(int, char**) {
+        cout << "abc";
+        return 0;
+    }
+}
---
+++ C++/HKOI/S191/unstoppable_onslaught.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace s191 {
+    int main(int, char**);
+}
